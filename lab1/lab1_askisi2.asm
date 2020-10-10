c184bd38afb1324558a02b0e2be922576372fd77
ptoloudis <57294317+ptoloudis@users.noreply.github.com>
2020-10-10 18:44:51 +0300

    Update lab1_askisi2.asm

--- lab1/lab1_askisi2.asm
+++ lab1/lab1_askisi2.asm
@@ -13,6 +13,48 @@ comma_msg: .asciiz ","
 main:
 add $s0,$0,$0 	# min
 add $s1,$0,$0	# max
+add $t0,$0,$0 	# N1
+add $t1,$0,$0	# N2
+
+for:
+li $v0, 4	#input N1	 
+la $a0, N1_msg 
+syscall
+li $v0,5
+syscall	
+move $t0, $v0 
+bltz $t0,final
+
+li $v0, 4	#input N2	 
+la $a0, N2_msg 
+syscall
+li $v0,5
+syscall	
+move $t1, $v0
+
+sub $t3,$t1,$t0
+sub $t4,$s1,$s0
+blt $t4,$t3,op_4
+
+blt $t0,$s0,min_op1
+blt $s1,$t1,max
+  
+
+option_1:
+add $s0,$0,$t0
+j for
+
+option_2:
+add $s1,$0,$t1
+j for
+
+option_3:
+j for
+
+option_4:
+add $s0,$0,$t0
+add $s1,$0,$t1
+j for
 
 
 final:
